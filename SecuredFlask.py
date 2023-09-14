ad6a3477c384de5cc1abefe06629d82a3f761c2f
MahdiST <84761387+FiredPhoenixIR@users.noreply.github.com>
2023-09-14 15:55:17 +0330

    Added comments

--- SecuredFlask.py
+++ SecuredFlask.py
@@ -15,3 +15,19 @@ talisman = Talisman(app, content_security_policy=csp)
 def index():
     """Base URL for our service"""
     return app.send_static_file("index.html")
+
+'''
+Added Headers :
+X-Frame-Options: SAMEORIGIN
+X-XSS-Protection: 1; mode=block
+X-Content-Type-Options: nosniff
+Content-Security-Policy: default-src 'self'
+Referrer-Policy: strict-origin-when-cross-origin
+'''
+'''
+You can also use the @talisman decorator 
+to gain more control over security at the route level if needed.
+'''
+'''
+Talisman also ensures that your domain uses HTTPS instead of HTTP
+'''
